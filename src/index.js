9ee075f2990f6105e2fe7a6deff03aa1910c6444
Artem Gorbachev <Ag2003g@yandex.ru>
2022-02-24 00:34:47 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let reverseN = ''
+    for(let digit of String(Math.abs(n))){
+        reverseN = digit + reverseN
+    }
+    return Number(reverseN)
 }
